f85d14a740f0292f9b3e0229a75bd7f95558c0d8
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2026-02-04 12:47:46 +0700

    still fixing..

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify"; // Import the adapter
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server", // Enable server-side rendering
+  adapter: netlify(), // Use Netlify adapter
+});
